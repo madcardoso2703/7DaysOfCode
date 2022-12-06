5a8b695bcb1e4fe4bdd35372f7149bb4cfaa47ad
Michel Adriano Cardoso <115435760+madcardoso2703@users.noreply.github.com>
2022-12-06 12:43:12 -0300

    finalizado com sucesso. desaf io resolvido

--- Desafio1.js
+++ Desafio1.js
@@ -1,1 +1,26 @@
+// variaveis
+let nA= 1
+let sB = '1'
+let nC = 30
+let sD = '30'
+let nE = 10
+let sF = '10'
 
+
+if (nA == sB) {
+  console.log('As variáveis nA e a sB tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis nA e a sB não tem o mesmo valor')
+}
+
+if (nC === sD) {
+  console.log('As variáveis nC e sD tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (nE == sF) {
+  console.log('As variáveis nE e sF tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis nE e sF não tem o mesmo valor')
+}
